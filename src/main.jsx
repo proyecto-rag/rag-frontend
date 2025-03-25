0ac19923b29f14a4b83694f2ce92f9e8b0bfbfa3
Bruno Jimenez <omarjm0910@gmail.com>
2025-03-25 11:19:03 -0600

    se agrega redux a la pagina web

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./styles.css";
 import { RagApp } from "./RagApp";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <RagApp />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <RagApp />
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
